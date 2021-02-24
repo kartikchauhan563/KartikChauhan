f66fc5ebc23a4e7d9d97eea2fcbe76da25d8dda0
KARTIK CHAUHAN <kartikchauhan563@gmail.com>
2021-02-23 19:04:51 -0500

    make another navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,19 @@
 <template>
   <div id="app">
-    <v-bottom-navigation :value="value" color="primary">
-      <v-btn>
-        <router-link to="/">Home</router-link>
-      </v-btn>
-      <!--
-      <v-btn>
-        <router-link to="/about">About</router-link>
-      </v-btn> -->
-
-      <v-btn>
-        <router-link to="/contact">Contact</router-link>
-      </v-btn>
-      <v-btn>
-        <router-link to="/portfolio">Portfolio</router-link>
-      </v-btn>
-      <v-btn>
-        <a href="https://fetchnews.herokuapp.com/">Fetch News App</a>
-      </v-btn>
-    </v-bottom-navigation>
+    <Navigation />
 
     <router-view />
-    <!-- <v-footer dark padless>
-      <v-card flat tile class="indigo lighten-1 white--text text-center">
-        <v-card-text>
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4 white--text"
-            icon
-          >
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-text>
 
-        <v-card-text class="white--text pt-0">
-          Experienced WordPress Developer and Front End Developer with a
-          demonstrated history of working in Information Technology. Skilled in
-          Special Purpose Web-Development, FrontEnd Development, Android
-          Development,Manual Testing, C,& Java.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-text class="white--text">
-          {{ new Date().getFullYear() }} —
-          <strong>Designed By Kartik Chauhan</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer> -->
     <Footer />
   </div>
 </template>
 <script>
 import Footer from "@/components/footer.vue";
+import Navigation from "@/components/navigation.vue";
 export default {
   name: "app",
   components: {
+    Navigation,
     Footer,
   },
 };
---
+++ src/components/navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-bottom-navigation :value="value" color="primary">
+    <v-btn>
+      <router-link to="/">Home</router-link>
+    </v-btn>
+    <!--
+      <v-btn>
+        <router-link to="/about">About</router-link>
+      </v-btn> -->
+
+    <v-btn>
+      <router-link to="/contact">Contact</router-link>
+    </v-btn>
+    <v-btn>
+      <router-link to="/portfolio">Portfolio</router-link>
+    </v-btn>
+    <v-btn>
+      <a href="https://fetchnews.herokuapp.com/">Fetch News App</a>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
